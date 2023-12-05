23d6a5f7c238942bcc391c94d48aa264485f2b8c
willieochoa <willieochoa+ga@gmail.com>
2023-12-04 16:19:15 -0800

    Expo React Native set up and initial base functionality. Created tarotDeck data. Basic display on screen of cards

--- App.js
+++ App.js
@@ -1,10 +1,51 @@
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import tarotCards from './tarotCards';
 
 export default function App() {
+  const [drawnCards, setDrawnCards] = useState([]);
+
+  const shuffleDeck = () => {
+    // Shuffle the tarotCards array
+    for (let i = tarotCards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [tarotCards[i], tarotCards[j]] = [tarotCards[j], tarotCards[i]];
+    }
+  };
+
+  const drawCard = (index) => {
+    const card = tarotCards.shift();
+    let newDrawnCards = [...drawnCards];
+    newDrawnCards[index] = { ...card, reversed: Math.random() < 0.5 };
+    setDrawnCards(newDrawnCards);
+  };
+  
+  const resetDeck = () => {
+    setDrawnCards([]);
+    // Reinitialize the deck (you might need to import the original deck again)
+    shuffleDeck();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={shuffleDeck} style={styles.shuffleButton}>
+        <Text>Shuffle Deck</Text>
+      </TouchableOpacity>
+  
+      <View style={styles.cardGrid}>
+        {[0, 1, 2].map((index) => (
+          <TouchableOpacity key={index} onPress={() => drawCard(index)} style={styles.cardContainer}>
+            <Text style={styles.cardText}>{drawnCards[index] ? `${drawnCards[index].name} ${drawnCards[index].reversed ? '(Upside down)' : ''}` : `Card ${index + 1}`}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+  
+      <TouchableOpacity onPress={resetDeck} style={styles.resetButton}>
+        <Text>Reset</Text>
+      </TouchableOpacity>
+  
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +56,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  shuffleButton: {
+    // Styles for the shuffle button
+  },
+  resetButton: {
+    // Styles for the reset button
+  },
+  cardGrid: {
+    flexDirection: 'row',
     justifyContent: 'center',
   },
+  cardContainer: {
+    // Styles for each card container
+  },
+  cardText: {
+    // Styles for the text inside each card container
+  },
+  // ... other styles
 });
