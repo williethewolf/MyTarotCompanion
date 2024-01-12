70f34a11d714ff698b018bc41162ab016dd3829d
willieochoa <willieochoa+ga@gmail.com>
2024-01-12 13:16:13 -0800

    Navigation routing and user log in check

--- app/index.js
+++ app/index.js
@@ -1,14 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Redirect } from 'expo-router'
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import { Redirect } from 'expo-router';
+import firebase from '../firebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth'
+import auth from '../firebaseConfig';
 
 const index = () => {
-  return (
-    
-    <Redirect href='../home/Dashboard'/>
-  )
-}
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default index
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser);
+      setLoading(false);
+    });
 
-const styles = StyleSheet.create({})
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <View style={styles.container}><ActivityIndicator size="large" /></View>;
+  }
+
+  if (user) {
+    // User is logged in, redirect to Dashboard
+    return <Redirect href='../home/Dashboard' />;
+  } else {
+    // User is not logged in, redirect to Login
+    return <Redirect href='../auth/LoginScreen' />; // Adjust the path to your Login screen
+  }
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '80%'
+  },
+});
