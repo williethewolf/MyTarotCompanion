f5f8d9aa09960ece1dcb9f79da6bd5c9d5c1c8a4
willieochoa <willieochoa+ga@gmail.com>
2024-01-20 11:16:13 -0800

    added fuzzy value to moon phase calculation to account for rounding errors and getting a more forgiving and probably accurate moon phase reading

--- components/MoonPhase.js
+++ components/MoonPhase.js
@@ -2,56 +2,58 @@ import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import SunCalc from 'suncalc';
 
-const MoonPhase = ({ display = "both",  fontSize = 55 }) => {
+const MoonPhase = ({ display = "both", fontSize = 55 }) => {
     const [moonPhase, setMoonPhase] = useState({ name: '', symbol: '' });
 
-  useEffect(() => {
-    const getMoonPhase = () => {
-      const now = new Date();
-      const phase = SunCalc.getMoonIllumination(now).phase;
-      
-      let name, symbol;
+    useEffect(() => {
+        const getMoonPhase = () => {
+            const now = new Date();
+            const phase = SunCalc.getMoonIllumination(now).phase;
+            console.log(phase);
+            
+            let name, symbol;
+            const epsilon = 0.03; // Threshold for fuzzy comparison
 
-      if (phase === 0) {
-        name = 'New Moon';
-        symbol = '🌑';
-      } else if (phase < 0.25) {
-        name = 'Waxing Crescent';
-        symbol = '🌒';
-      } else if (phase === 0.25) {
-        name = 'First Quarter';
-        symbol = '🌓';
-      } else if (phase < 0.5) {
-        name = 'Waxing Gibbous';
-        symbol = '🌔';
-      } else if (phase === 0.5) {
-        name = 'Full Moon';
-        symbol = '🌕';
-      } else if (phase < 0.75) {
-        name = 'Waning Gibbous';
-        symbol = '🌖';
-      } else if (phase === 0.75) {
-        name = 'Last Quarter';
-        symbol = '🌗';
-      } else {
-        name = 'Waning Crescent';
-        symbol = '🌘';
-      }
+            if (Math.abs(phase - 0) < epsilon) {
+                name = 'New Moon';
+                symbol = '🌑';
+            } else if (phase < 0.25 - epsilon) {
+                name = 'Waxing Crescent';
+                symbol = '🌒';
+            } else if (Math.abs(phase - 0.25) < epsilon) {
+                name = 'First Quarter';
+                symbol = '🌓';
+            } else if (phase < 0.5 - epsilon) {
+                name = 'Waxing Gibbous';
+                symbol = '🌔';
+            } else if (Math.abs(phase - 0.5) < epsilon) {
+                name = 'Full Moon';
+                symbol = '🌕';
+            } else if (phase < 0.75 - epsilon) {
+                name = 'Waning Gibbous';
+                symbol = '🌖';
+            } else if (Math.abs(phase - 0.75) < epsilon) {
+                name = 'Last Quarter';
+                symbol = '🌗';
+            } else {
+                name = 'Waning Crescent';
+                symbol = '🌘';
+            }
 
-      setMoonPhase({ name, symbol });
-    };
+            setMoonPhase({ name, symbol });
+        };
 
-    getMoonPhase();
-  }, []);
+        getMoonPhase();
+    }, []);
 
-  const textStyle = { fontSize: fontSize } ;
-  return(
-  <View>
-      {display === "both" && <Text style={textStyle}>{moonPhase.symbol} {moonPhase.name}</Text>}
-      {display === "symbol" && <Text style={textStyle}>{moonPhase.symbol}</Text>}
-      {display === "name" && <Text style={textStyle}>{moonPhase.name}</Text>}
-    </View>
-    )
+    const textStyle = { fontSize: fontSize };
+    return (
+        <View>
+            {display === "both" && <Text style={textStyle}>{moonPhase.symbol} {moonPhase.name}</Text>}
+            {display === "symbol" && <Text style={textStyle}>{moonPhase.symbol}</Text>}
+            {display === "name" && <Text style={textStyle}>{moonPhase.name}</Text>}
+        </View>
+    );
 };
 
 export default MoonPhase;
