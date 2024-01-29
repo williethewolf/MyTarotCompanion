9727b84eab47fb5b6db228f001e7ea332dd934d3
willieochoa <willieochoa+ga@gmail.com>
2024-01-29 13:56:32 -0800

    First screen has been now replaced by the LogInScreen. Marked as OBSOLETE and retired.

---
+++ app/_OBSOLETE_FirstScreen.js
@@ -0,0 +1,49 @@
+// FirstScreen.js
+//
+//THIS IS OBSOLETE
+//
+import React, { useEffect, useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import { Link, useNavigation } from 'expo-router';
+import { Link,  } from 'expo-router';
+import firebase from '../firebaseConfig';
+import auth from '../firebaseConfig';
+
+const FirstScreen = ({ navigation }) => {
+    const [loading, setLoading] = useState(true);
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            if (user) {
+                // User is signed in, navigate to Home
+                navigation.navigate('../home/');
+            } else {
+                // No user is signed in, stay on this screen
+                setLoading(false);
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    if (loading) {
+        return <View><Text>Loading...</Text></View>;
+    }
+
+    return (
+        <View>
+            <Text>Welcome to the CassandrApp!</Text>
+            <Link href="../auth/LoginScreen">
+            <Text>Log In</Text>
+            </Link>
+            <Text>Don't have an account yet?</Text>
+            <Link href="../auth/SignUpScreen">
+            <Text>Sign Up</Text>
+            </Link>
+        </View>
+    );
+};
+
+export default FirstScreen;
