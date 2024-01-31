0bc3fbce765f263a90daa9e4476a078885b48654
willieochoa <willieochoa+ga@gmail.com>
2024-01-31 02:44:51 -0800

    Expo Incompatibility. Can't implement Firebase google account log in without ejecting. Commented and postponed functionality

--- components/auth.js
+++ components/auth.js
@@ -1,6 +1,15 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+//import { GoogleSignin } from 'react-native-google-signin/google-signin';
 
 const auth = getAuth();
+
+//UNCOMMENT TO IMPLEMENT GOOGLE SIGNIN
+// const WEB_CLIENT_ID = process.env.EXPO_PUBLIC_DAILY_HOROSCOPE_API_KEY
+
+// // Configure Google Sign
+// GoogleSignin.configure({
+//     webClientId: WEB_CLIENT_ID,
+//   });
 
 export const createUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
@@ -14,4 +23,19 @@ export const signOutUser = () => {
     return signOut(auth);
 };
 
+//UNCOMMENT TO IMPLEMENT GOOGLE SIGNIN
+// export const googleSignIn = async () => {
+//     try {
+//       // Get the users ID token
+//       const { idToken } = await GoogleSignin.signIn();
+  
+//       // Create a Google credential with the token
+//       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+//       // Sign-in the user with the credential
+//       return auth().signInWithCredential(googleCredential);
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
 
